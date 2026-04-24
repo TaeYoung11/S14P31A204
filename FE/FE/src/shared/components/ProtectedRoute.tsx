796f06109bec356e4652394923c9be44e92ff024
오지수 <151099185+ohjisu320@users.noreply.github.com>
2026-04-24 14:49:48 +0900

    [FE-S14P31A204-135] fix: 로그인/회원가입 라우팅과 인증 가드를 정리

--- FE/FE/src/shared/components/ProtectedRoute.tsx
+++ FE/FE/src/shared/components/ProtectedRoute.tsx
@@ -1,8 +1,8 @@
-import { Outlet } from 'react-router-dom'
+import { Navigate, Outlet } from 'react-router-dom'
+import { useAuthStore } from '@/shared/stores/authStore'
 
-// 인증된 사용자만 접근 가능한 라우트 가드
-// 토큰 없으면 /login 으로 리다이렉트
 export function ProtectedRoute() {
-// TODO: BE auth 구현 후 인증 체크 복구
+  const token = useAuthStore((state) => state.token)
+  if (!token) return <Navigate to="/login" replace />
   return <Outlet />
 }
