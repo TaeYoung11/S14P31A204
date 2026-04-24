75f52333f87a1a2bb9b6114ec406da649cdaa65f
김태영 <57125988+pieangel11@users.noreply.github.com>
2026-04-24 10:07:20 +0900

    [FE-S14P31A204-104] fix: auth 제한 route 수정

--- FE/FE/src/shared/components/ProtectedRoute.tsx
+++ FE/FE/src/shared/components/ProtectedRoute.tsx
@@ -3,7 +3,6 @@ import { Outlet } from 'react-router-dom'
 // 인증된 사용자만 접근 가능한 라우트 가드
 // 토큰 없으면 /login 으로 리다이렉트
 export function ProtectedRoute() {
-  const token = useAuthStore((s) => s.token)
-  if (!token) return <Navigate to="/login" replace />
+// TODO: BE auth 구현 후 인증 체크 복구
   return <Outlet />
 }
