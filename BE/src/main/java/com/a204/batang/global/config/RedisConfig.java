ddba04ef3a410db38eca79ee311a74748dad46cb
박연준 <129067071+QUWR@users.noreply.github.com>
2026-04-30 09:00:58 +0900

    [BE-S14P31A204-126] feat: redis config

--- BE/src/main/java/com/a204/batang/global/config/RedisConfig.java
+++ BE/src/main/java/com/a204/batang/global/config/RedisConfig.java
@@ -4,29 +4,29 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 /**
  * Redis 연결 및 직렬화 방식을 설정한다.
- * 키는 문자열, 값은 JSON 직렬화를 기본으로 사용한다.
+ * 키/값 모두 문자열 직렬화를 사용해 범용 역직렬화 공격면을 줄인다.
  */
 @Configuration
 public class RedisConfig {
 
     /**
      * RedisTemplate 기본 빈을 등록한다.
-     * 리스트/해시/문자열 공통 연산에서 동일한 직렬화 정책을 사용하기 위해 명시적으로 설정한다.
+     * 현재 undo/redo 시나리오에서 문자열(URL/리비전/커서) 중심으로 저장하므로
+     * 값 직렬화도 String으로 고정해 안전성과 단순성을 우선한다.
      *
      * @param connectionFactory Redis 연결 팩토리
      * @return 설정된 RedisTemplate
      */
     @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
-        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
 
         StringRedisSerializer keySerializer = new StringRedisSerializer();
-        GenericJackson2JsonRedisSerializer valueSerializer = new GenericJackson2JsonRedisSerializer();
+        StringRedisSerializer valueSerializer = new StringRedisSerializer();
 
         redisTemplate.setConnectionFactory(connectionFactory);
         redisTemplate.setKeySerializer(keySerializer);
