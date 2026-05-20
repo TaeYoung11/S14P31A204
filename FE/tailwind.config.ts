afa4af174e4897c49af451048671c460ddcfd982
오지수 <151099185+ohjisu320@users.noreply.github.com>
2026-05-20 15:43:06 +0900

    [FE-S14P31A204-447] fix: 에디터 Pretendard 폰트 적용

--- FE/tailwind.config.ts
+++ FE/tailwind.config.ts
@@ -3,7 +3,26 @@ import type { Config } from 'tailwindcss'
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: [
+          'Pretendard Variable',
+          'Pretendard',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          'system-ui',
+          'sans-serif',
+        ],
+        mono: [
+          'Pretendard Variable',
+          'Pretendard',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          'system-ui',
+          'sans-serif',
+        ],
+      },
+    },
   },
   plugins: [],
 } satisfies Config
